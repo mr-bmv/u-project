3b2c34a37cc52e2626e07fce00a34aed591bdac1
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-15 11:29:09 +0300

    Добавлены темы, контекст тем, хук тем.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
-import { Suspense } from "react";
+import "./styles/index.scss";
+import { Suspense, useState } from "react";
 import MainPage from "./pages/MainPage/MainPage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<h1>Loading ...</h1>}>
