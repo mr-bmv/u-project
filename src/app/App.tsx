e778cda110236d5dc60cf281bfe5f16ed5968a28
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-15 17:25:22 +0300

    ThemeSwitcher свитчер тем кастомный

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,11 @@ import { AppRouter } from "./providers/routes";
 import "./styles/index.scss";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
       <Navbar />
-      <button onClick={toggleTheme}>TOGGLE</button>
-
       <AppRouter />
     </div>
   );
