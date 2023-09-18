bd00ef7b6f736d023efb8ae483f09dc7903d8fb5
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-18 19:20:22 +0300

    перенес ссылки из __Navbar__ в __Sidebar__

--- src/widget/Navbar/ui/Navbar.tsx
+++ src/widget/Navbar/ui/Navbar.tsx
@@ -1,7 +1,5 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
-import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink/AppLink';
-import { t } from 'i18next';
 
 interface NavbarProps {
   className?: string;
@@ -10,18 +8,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className || ''])}>
-      <div className={cls.links}>
-        <AppLink
-          theme={AppLinkTheme.SECONDARY}
-          to={'/'}
-          className={cls.mainLink}
-        >
-          {t('Главная')}
-        </AppLink>
-        <AppLink theme={AppLinkTheme.RED} to={'/about'}>
-          {t('О сайте')}
-        </AppLink>
-      </div>
+      <div className={cls.links}></div>
     </div>
   );
 };
--- src/widget/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widget/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,6 +4,11 @@ import { useState } from 'react';
 import { ThemeSwitcher } from '@/widget/ThemeSwitcher';
 import { LangSwitcher } from '@/widget/LangSwitcher';
 import { Button, ButtonSize, ThemeButton } from '@/shared/ui/Button/Button';
+import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink/AppLink';
+import { RoutePath } from '@/shared/config/routConfig/routConfig';
+import { useTranslation } from 'react-i18next';
+import AboutIcon from '@/shared/assets/icons/about-20-20.svg';
+import MainIcon from '@/shared/assets/icons/main-20-20.svg';
 
 interface SidebarProps {
   className?: string;
@@ -11,6 +16,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className = '' }: SidebarProps) => {
   const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation();
 
   const onToggle = () => {
     setCollapsed((prev) => !prev);
@@ -31,6 +37,30 @@ export const Sidebar = ({ className = '' }: SidebarProps) => {
       >
         {collapsed ? '>' : '<'}
       </Button>
+      {/* <Suspense fallback> */}
+      <div className={cls.items}>
+        <div className={cls.item}>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.main}
+            className={cls.item}
+          >
+            <MainIcon className={cls.icon} />
+            <span className={cls.link}>{t('Главная')}</span>
+          </AppLink>
+        </div>
+        <div className={cls.item}>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.about}
+            className={cls.item}
+          >
+            <AboutIcon className={cls.icon} />
+            <span className={cls.link}> {t('О сайте')}</span>
+          </AppLink>
+        </div>
+      </div>
+      {/* </Suspense> */}
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} short={collapsed} />
