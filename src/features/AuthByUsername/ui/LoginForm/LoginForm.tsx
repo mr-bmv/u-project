123477cbeff6aa22bedb311c26e1dbbd5388c92d
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-20 17:48:38 +0300

    доработали feature AuthByUsername асинхронными запросами

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,30 +1,77 @@
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Button } from '@/shared/ui/Button/Button';
-import cls from './LoginForm.module.scss';
+import { Button, ThemeButton } from '@/shared/ui/Button/Button';
 import { Input } from '@/shared/ui/Input/Input';
+import { useDispatch, useSelector } from 'react-redux';
+import { memo, useCallback } from 'react';
+import { loginByUsername } from '../../model/services/loginByUsername/loginByUsername';
+import { loginActions } from '../../model/slice/loginSlice';
+import cls from './LoginForm.module.scss';
+import { getLoginState } from '../../model/selectors/getLoginState/getLoginState';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+import { LoginSchema } from '../../model/types/loginSchema';
+import { Text, TextTheme } from '@/shared/ui/Text/Text';
 
 interface LoginFormProps {
   className?: string;
 }
 
-export const LoginForm = ({ className = '' }: LoginFormProps) => {
+export const LoginForm = memo(({ className = '' }: LoginFormProps) => {
   const { t } = useTranslation();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const dispatch = useDispatch<ThunkDispatch<LoginSchema, unknown, any>>();
+  const { username, password, error, isLoading } = useSelector(getLoginState);
+
+  const onChangeUsername = useCallback(
+    (value: string) => {
+      dispatch(loginActions.setUsername(value));
+    },
+    [dispatch]
+  );
+
+  const onChangePassword = useCallback(
+    (value: string) => {
+      dispatch(loginActions.setPassword(value));
+    },
+    [dispatch]
+  );
+
+  const onLoginClick = useCallback(() => {
+    dispatch(loginByUsername({ username, password }));
+  }, [dispatch, password, username]);
 
   return (
     <div className={classNames(cls.LoginForm, {}, [className])}>
+      <Text title={t('Форма авторизации')} />
+      {error && (
+        <Text
+          text={t('Вы ввели неверный логин или пароль')}
+          theme={TextTheme.ERROR}
+        />
+      )}
       <Input
         autofocus
         type="text"
         className={cls.input}
         placeholder={t('Введите username')}
+        onChange={onChangeUsername}
+        value={username}
       />
       <Input
         type="text"
         className={cls.input}
         placeholder={t('Введите пароль')}
+        onChange={onChangePassword}
+        value={password}
       />
-      <Button className={cls.loginBtn}>{t('Войти')}</Button>
+      <Button
+        theme={ThemeButton.OUTLINE}
+        className={cls.loginBtn}
+        onClick={onLoginClick}
+        disabled={isLoading}
+      >
+        {t('Войти')}
+      </Button>
     </div>
   );
-};
+});
