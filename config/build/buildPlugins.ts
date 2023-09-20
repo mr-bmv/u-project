26b8a126dce2283d5009af9f6a4ed6482ad7e650
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-20 22:02:26 +0300

    webpack-bundle-analyzer и axios

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,17 +1,18 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
-    new HtmlWebpackPlugin(
+  const plugins = [
+    new HtmlWebpackPlugin({
       // куда будут встраиваться все скрипты
-      { template: paths.html }
-    ),
+      template: paths.html,
+    }),
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash:8].css',
@@ -21,7 +22,13 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    // отрисовывать изменения без перезагрузки страницы
-    new webpack.HotModuleReplacementPlugin(),
   ];
+  plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+
+  if (isDev) {
+    // plugins.push(new ReactRefreshWebpackPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return plugins;
 }
