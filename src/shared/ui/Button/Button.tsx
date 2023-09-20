106fc11d1ea3261ce22faa238c94f133f40164f3
Mike Botkin <mr.m.botkin@gmail.com>
2023-09-20 17:23:06 +0300

    __Button__ доработана

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -16,11 +16,13 @@ export enum ButtonSize {
   L = 'size_l',
   XL = 'size_xl',
 }
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ThemeButton;
   square?: boolean;
   size?: ButtonSize;
+  disabled?: boolean;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
@@ -29,17 +31,23 @@ export const Button: FC<ButtonProps> = (props) => {
     children,
     theme = '',
     square = false,
+    disabled = false,
     size = ButtonSize.M,
     ...otherProps
   } = props;
 
-  const mods = {
+  const mods: Record<string, boolean> = {
+    [cls[theme]]: true,
     [cls.square]: square,
     [cls[size]]: true,
+    [cls.disabled]: disabled,
   };
+
   return (
     <button
-      className={classNames(cls.Button, mods, [className, cls[theme]])}
+      type="button"
+      className={classNames(cls.Button, mods, [className])}
+      disabled={disabled}
       {...otherProps}
     >
       {children}
